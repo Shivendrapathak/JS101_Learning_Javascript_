969df7d72b44dd68eb36d77918ad19b6ee68432b
Shivam Dwivedi <pathakshivendra68@gmail.com>
2022-09-04 21:16:23 +0530

    variables

---
+++ variables/problem2.js
@@ -0,0 +1,9 @@
+let Subject = "Englīsh";
+let marks = 75;
+let passing_marks = 70
+if (Subject == "Engkish" && marks>=passing_marks){
+  console.log("Pass")
+  if(Subject == "Engkish"&&marks<=passing_marks){
+    console.log("Fail")
+  }
+}
