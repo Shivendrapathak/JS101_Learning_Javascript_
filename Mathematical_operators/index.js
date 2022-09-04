e935c05eaed44f0055fb910c3e65022097107ea6
Shivam Dwivedi <pathakshivendra68@gmail.com>
2022-09-04 22:14:15 +0530

    Mathematical_operators

---
+++ Mathematical_operators/index.js
@@ -0,0 +1,11 @@
+let Subject = "Englīsh";
+let marks = 71;
+let passing_marks = 70;
+if ( marks>passing_marks){
+  console.log("Pass")
+  if (marks<passing_marks){
+    console.log("Fail")
+  }
+}
+"marks>passing_marks" ? console.log("Pass") 
+  : "marks<passing_marks" ?  console.log("Fail")
---
+++ Mathematical_operators/problem3.js
@@ -0,0 +1,14 @@
+let one = 1;
+let two = 2;
+let three = 3;
+let four = 4;
+let five = 5;
+let six = 6;
+
+one = 1;
+two = 4;
+three = 9; 
+four = 12;
+five = 10;
+six = 12;
+console.log(one+two+three+four+five+six);
